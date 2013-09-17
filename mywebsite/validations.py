cfdbe4a351125bd1ff5b2b04ba7c467c46cce889
macha <macha@machas-MacBook-Pro.local>
2013-09-17 19:19:25 -0400

    added nike app client

---
+++ mywebsite/validations.py
@@ -0,0 +1,117 @@
+'''
+Created on Mar 19, 2013
+
+@author: macha
+'''
+
+_special_characters = ['@','#','$','%','^','&','*','!','~','`','[',']','{','}','|','\\','?','/',',','.','<','>']
+_numbers = ['0','1','2','3','4','5','6','7','8','9']
+
+class Validations(object):
+    """
+validation class to validate user inputs
+"""
+
+    def check_valid_userName(self,name):
+        """
+method to check whether user has given a valid user name
+@returns False if it contains a special character or lenth is zero or is None
+@returns True, otherwise
+"""
+        validation = Validations()
+        if validation.is_None(name):
+            return False
+        elif validation.is_length_zero(name):
+            return False
+        elif validation.contains_special_character(name):
+            return False
+        return True
+
+    def check_valid_userId(self,name):
+        """
+method to check if use has provided a valid ID
+@returns True, if userId starts with an alphabet and length >= 9
+@returns False otherwise
+"""
+        validation = Validations()
+        if validation.is_None(name):
+            return False
+        elif validation.is_length_zero(name) == True or len(name) > 9:
+            print 'i am here'
+            return False
+        elif validation.starts_with_number(name):
+            return False
+        elif validation.starts_with_special_character(name):
+            return False
+        return True
+
+    def contains_special_character(self,name):
+        """
+this method checks if the name contains a special character in it
+@returns True, if it contains
+@returns False otherwise
+"""
+        for char in _special_characters:
+            if char in name:
+                return True
+        return False
+
+    def starts_with_number(self,name):
+        """
+this method checks if the name starts with a number
+@returns True, if it contains
+@returns False otherwise
+"""
+        if name[0] in _numbers:
+            return True
+        return False
+
+    def starts_with_special_character(self,name):
+        """
+this method checks if the name starts with a special character
+@returns True, if it contains
+@returns False otherwise
+"""
+        if name[0] in _special_characters:
+            return True
+        return False
+
+    def is_length_zero(self,name):
+        """
+this method checks if the length of the name is zero
+@returns True, if it is zero
+@returns False otherwise
+"""
+        if len(name) == 0:
+            return True
+        return False
+
+    def is_None(self,name):
+        """
+this method checks if the name is None
+@returns True, if it is None
+@returns False otherwise
+"""
+        if name == None:
+            return True
+        return False
+
+
+validation = Validations()
+#print validation.check_valid_userName("nawe")
+
+
+# error = "no error"
+# return True,error
+
+#validation = Validations()
+#result = validation.check_valid_username("name") or len(name) > 9
+#print result[1]
+
+
+"""
+if validation.is_None(name):
+print 'i am here'
+return False
+"""
+
--- mywebsite/views.py
+++ mywebsite/views.py
@@ -1,36 +1,70 @@
+
 # Create your views here.
-
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,HttpResponse
+from validations import Validations
+import requests
+from django.utils import simplejson
+import AppClientConstants
 from django.template import RequestContext
-import Constants
 
+
+
+
+# object for Validations class in validations module
+validation = Validations()
 
 def home(request):
     return render_to_response("myhomepage.html")
 
-def regions(request):
-    region_list = Constants.regions
-    context = {
-       'regions' : region_list
-    }
-    return render_to_response("regions.html",context, context_instance=RequestContext(request))
-    
-def region(request,region):
-    if region == "telangana":
-        district_list = Constants.telangana_districts
+def nike(request):
+    """
+method to return the home page
+"""
+    return render_to_response('CreateUser.html',context_instance=RequestContext(request))
+
+def create_user(request):
+    """
+method to create a user
+"""
+    name = request.POST['userName']
+    uId = request.POST['userId']
+    if validation.check_valid_userName(name) == True:
+        userName = name
     else:
-        district_list = Constants.regions
-    context = {
-        'list':district_list
-    }
-    return render_to_response("success.html",context,context_instance=RequestContext(request))       
+        error = AppClientConstants.VALID_NAME
+        return render_to_response('CreateUser.html',{'error':error})
+    if validation.check_valid_userId(uId) == True:
+        userId = uId
+    else:
+        error = AppClientConstants.VALID_USER_ID
+        return render_to_response('CreateUser.html',{'error':error})
+    payload = {"name":userName,"id":userId}
+    url = "http://nike-todo.aws.af.cm/users/"
+    result = requests.post(url, data=payload)
+    result_json = result.json()
+    if result_json.has_key('message'):
+        message = result_json['message']
+        message_type =result_json['messageType']
+        return render_to_response('result.html',{'message':message,'messageType':message_type})
+    else:
+        data = result_json['data']
+        name = data['name']
+        userId = result_json['id']
+        return render_to_response('user_result.html',{'name':name,'userId':userId})
 
-    
-def districts(request):
-    district_list = Constants.all_districts
-    return render_to_response("districts.html",{"districts":district_list})
+def todo(request):
+    return render_to_response('CreateTodo.html',{},context_instance=RequestContext(request))
 
-def district(request,district):
-    if district == "hyderabad":
-        return render_to_response("hyderabad.html")
-    return render_to_response("other.html")
+
+def createtodo(request):
+    userId = request.POST['userId']
+    return render_to_response('ResultTodo.html',{"userId":userId})
+
+def list_todos():
+    pass
+
+
+def do_not_delete():
+    user_dictionary = {"userName":"kennedy","id":"123"}
+    data = simplejson.dumps(user_dictionary)
+    return HttpResponse(data,mimetype='application/json')
