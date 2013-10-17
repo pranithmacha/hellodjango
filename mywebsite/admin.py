37338f46ae872e17baea867b73fb84c62eba42f9
macha <macha@machas-MacBook-Pro.local>
2013-10-17 19:49:19 -0400

    new tables

--- mywebsite/admin.py
+++ mywebsite/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from models import Projects
 
 
-#admin.site.register(Projects)
+admin.site.register(Projects)
---
+++ mywebsite/models.py
@@ -0,0 +1,15 @@
+
+from django.db import models
+
+# Create your models here.
+
+class Projects(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    techstack = models.TextField()
+    git_link = models.TextField()
+    website_link = models.TextField()
+    
+    class Meta:
+        verbose_name_plural = "Projects"
+  
