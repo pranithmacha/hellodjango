c61c62106cb67aa989d7beb21e3f9bf9b2e7ba06
macha <macha@machas-MacBook-Pro.local>
2013-10-22 18:17:25 -0400

    send mail

--- mywebsite/views.py
+++ mywebsite/views.py
@@ -11,13 +11,8 @@ def home(request):
     return render_to_response("myhomepage.html")
 
 def contact(request):
-    """
-    #function to render Contact form. 
-    #@renders an empty form if get request is made
-    #@renders form with errors is form is not valid
-   # @renders result page if no errors are found
-    """
-    if request.method == 'POST': # If the form has been submitted...
+    if request.method == 'POST': 
+        # If the form has been submitted...
         form = ContactForm(request.POST) # A form bound to the POST data
         if form.is_valid():
              # All validation rules pass
@@ -32,10 +27,7 @@ def contact(request):
             return render_to_response('result.html',{'message':'thank you for posting'})
     else:
         form = ContactForm() # An unbound form
-
-    return render(request, 'contact.html', {
-        'form': form,
-    })
+    return render(request, 'contact.html', {'form': form})
     
 def about_me(request):
     return render_to_response("about.html")
