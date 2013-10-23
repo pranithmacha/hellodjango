8ca0da6dc634bc682448616506d1d892f906fbf8
macha <macha@machas-MacBook-Pro.local>
2013-10-22 23:27:42 -0400

    changes to form css

--- mywebsite/MyForms.py
+++ mywebsite/MyForms.py
@@ -3,5 +3,5 @@ from django import forms
 class ContactForm(forms.Form):
     sender = forms.EmailField()
     subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea(attrs={"class":"span3","placeholder":"type your name here .."}))
+    message = forms.CharField(widget=forms.Textarea(attrs={"class":"span3","placeholder":"type your name here ..",'size':'40'}))
     
