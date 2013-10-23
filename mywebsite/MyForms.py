bdb30bfd0c331c7842ee6e298e224092d067aa4a
macha <macha@machas-MacBook-Pro.local>
2013-10-22 22:50:00 -0400

    changes to form css

--- mywebsite/MyForms.py
+++ mywebsite/MyForms.py
@@ -5,6 +5,3 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea(attrs={class="span3", placeholder="type your message here .."}))
     
- 
-    name = forms.CharField(
-                 widget=forms.TextInput(attrs={'class':'special'}))
