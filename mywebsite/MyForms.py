20aa5b090cac0bea7dae95278d1e7b86139192d2
macha <macha@machas-MacBook-Pro.local>
2013-10-25 20:04:20 -0400

    textarea size changes

--- mywebsite/MyForms.py
+++ mywebsite/MyForms.py
@@ -3,5 +3,5 @@ from django import forms
 class ContactForm(forms.Form):
     sender = forms.EmailField()
     subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea(attrs={"class":"span3","placeholder":"type your name here ..",'cols':'40',"rows":"5"}))
+    message = forms.CharField(widget=forms.Textarea(attrs={"class":"span3","placeholder":"type your name here ..",'cols':'80',"rows":"5"}))
     
