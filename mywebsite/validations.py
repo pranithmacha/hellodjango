b85119a12a95741077899a028bb85c56fdde3f97
macha <macha@machas-MacBook-Pro.local>
2014-03-30 19:33:02 -0400

    test

--- mywebsite/validations.py
+++ mywebsite/validations.py
@@ -9,8 +9,8 @@ _numbers = ['0','1','2','3','4','5','6','7','8','9']
 
 class Validations(object):
     """
-validation class to validate user inputs
-"""
+    validation class to validate user inputs
+    """
 
     def check_valid_userName(self,name):
         """
--- mywebsite/views.py
+++ mywebsite/views.py
@@ -1,6 +1,5 @@
 # Create your views here.
 from django.shortcuts import (render_to_response,HttpResponse,render)
-import AppClientConstants
 from django.template import RequestContext
 from MyForms import ContactForm
 from models import MyProjects
